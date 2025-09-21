cf73afa060448afb9b9c64c45f8c85e593d91082
JiyaMavani <mavanijiya2110@gmail.com>
2025-09-21 22:16:54 +0530

    added new file

---
+++ DevelopmentTimer.UI/State/AppState.cs
@@ -0,0 +1,8 @@
+﻿namespace DevelopmentTimer.UI.State
+{
+    public class AppState
+    {
+        public static string? CurrentRole { get; set; }
+        public static string? Username { get; set; }
+    }
+}
